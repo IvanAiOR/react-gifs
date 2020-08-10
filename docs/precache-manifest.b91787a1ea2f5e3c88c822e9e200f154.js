bf547ee76ee06c493c2e187d9634a35aa8dddf95
IvanAiOr <irivascas@gmail.com>
2020-08-09 20:01:23 -0400

    intento de subir docs

---
+++ docs/precache-manifest.b91787a1ea2f5e3c88c822e9e200f154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b9b4e7301a4c080a5dae4921aa0cdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40acfa86f2b45843c064",
+    "url": "/static/css/main.c1658da3.chunk.css"
+  },
+  {
+    "revision": "28a892cf9238edc274c3",
+    "url": "/static/js/2.43af34a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43af34a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40acfa86f2b45843c064",
+    "url": "/static/js/main.5ffdad21.chunk.js"
+  },
+  {
+    "revision": "eb5011bc41922cdf1a2f",
+    "url": "/static/js/runtime-main.a0c3b904.js"
+  }
+]);
